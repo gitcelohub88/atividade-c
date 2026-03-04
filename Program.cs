38a407ee174270b3d003e1a45c8b72a91d8b9559
gitcelohub88 <marcelogavranicb@gmail.com>
2026-03-04 17:13:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace testreadline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+
+            //Console.WriteLine("Enter your age:");
+            // int age = Console.ReadLine();
+            // Console.WriteLine("Your age is:" + age);
+            // The error message will be something like this:
+
+
+
+
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine ());
+            Console.WriteLine("Your age is: " + age);
+
+
+        }
+    }
+}
