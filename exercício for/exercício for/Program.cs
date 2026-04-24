1a18adc09e2fff61ad6a5025ea88e361ad29992e
gitcelohub88 <marcelogavranicb@gmail.com>
2026-04-24 17:17:48 -0300

    Add files via upload

---
+++ exercício for/exercício for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercício_for
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numero = 5;
+
+            Console.WriteLine($"--- Tabuada do {numero} ---");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+
+            Console.WriteLine("------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
