1746037ebcbe4ad49157816375372cff869a7486
gitcelohub88 <marcelogavranicb@gmail.com>
2026-04-24 15:23:14 -0300

    Add files via upload

---
+++ exercício break continue/exercício break continue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercício_break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            while (numero < 10)
+                
+            {
+                if (numero == 5)
+                {
+                    numero++;
+                    continue;
+                    
+                }
+                else if (numero == 9)
+                {
+                    break;
+                }
+                
+                Console.WriteLine(numero);
+                numero++;
+            }
+                
+        }
+    }
+}
---
+++ exercício if/exercício if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercício_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira sua idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if(idade<18)
+            {
+                Console.WriteLine("menor de idade");
+
+            } 
+            else if (idade>=18 && idade <= 59)
+            {
+                Console.WriteLine("adulto"); 
+            }
+            else
+            {
+                Console.WriteLine("idoso");
+            }
+        }
+    }
+}
