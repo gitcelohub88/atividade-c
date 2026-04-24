302c963a4de4ad1618110cab5f45cbb09df51487
gitcelohub88 <marcelogavranicb@gmail.com>
2026-04-24 17:12:10 -0300

    Add files via upload

---
+++ exercício short hand/exercício short hand/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercício_short_hand
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // O Operador Ternário funciona assim: condição ? se_verdade : se_falso
+            string resultado = (numero % 2 == 0) ? "número par" : "número ímpar";
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
