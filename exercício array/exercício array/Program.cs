b2384c47fb244bdfdf58d6758fe5a59a08c367f5
gitcelohub88 <marcelogavranicb@gmail.com>
2026-04-24 17:29:29 -0300

    Add files via upload

---
+++ exercício array/exercício array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercício_array
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] nomes = { "Ana", "Bruno", "Carla", "Diego", "Elena" };
+
+            Console.WriteLine("--- Todos os nomes ---");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            Console.WriteLine("\n--- Seleção específica ---");
+
+            Console.WriteLine("Primeiro nome: " + nomes[0]);
+
+            Console.WriteLine("Último nome: " + nomes[nomes.Length - 1]);
+
+            Console.ReadLine();
+        }
+    }
+}
