87cc79077604b1f6cbc7c278ee8febf9f636d505
gitcelohub88 <marcelogavranicb@gmail.com>
2026-04-24 17:23:12 -0300

    Add files via upload

---
+++ exercício while/exercício while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercício_while
+{
+    class Program
+    {
+        static void Main()
+        {
+            int contador = 1;
+
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+
+                contador++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
